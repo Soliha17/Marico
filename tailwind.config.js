7f467c9e00782f76bd87005c135c56ebe69ecb8e
Soliha <solyatursuntasheva@gmail.com>
2024-03-11 00:18:07 +0500

    Configure: tailwind and jsconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      green: "#0FC65C",
+      white: "#FFFFFF",
+      blue: {
+        DEFAULT: "#3C82F4",
+        dark: "#326CF9",
+      },
+      gray: {
+        light: "#707070",
+        DEFAULT: "#919191",
+        dark: "#2D2D2D",
+      },
+      black: {
+        light: "#0E0E0E",
+        DEFAULT: "#000000",
+      },
+    },
   },
   plugins: [],
 };
